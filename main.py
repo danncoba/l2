f0412df7a256f3e03208ab3e8c76f4ce04a728d9
Danijel Djuric <danijeldjuric@M-D91JF2XN6K.local>
2025-06-23 11:31:54 +0200

    Added initial monitoring setup

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import logging
+import time
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk.resources import Resource
 
 from routers.configs import config_router
 from routers.grades import grades_router
@@ -9,6 +20,43 @@ from routers.notifications import notifications_router
 from routers.profile import profile_router
 from routers.skills import skills_router
 from routers.users import users_router
+
+# Configure resource
+resource = Resource.create({
+    "service.name": "htec-morpheus",
+    "service.version": "1.0.0",
+    "deployment.environment": "development"
+})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+otlp_trace_exporter = OTLPSpanExporter(
+    endpoint="http://localhost:4317",
+    insecure=True
+)
+
+# Add span processor
+span_processor = BatchSpanProcessor(otlp_trace_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+# Set up logging
+logger_provider = LoggerProvider(resource=resource)
+
+# OTLP Log Exporter
+otlp_log_exporter = OTLPLogExporter(
+    endpoint="http://localhost:4317",
+    insecure=True
+)
+
+# Add log processor
+log_processor = BatchLogRecordProcessor(otlp_log_exporter)
+logger_provider.add_log_record_processor(log_processor)
+
+# Set up Python logging to use OpenTelemetry
+handler = LoggingHandler(level=logging.INFO, logger_provider=logger_provider)
+logging.getLogger().addHandler(handler)
+logging.getLogger().setLevel(logging.INFO)
 
 app = FastAPI(
     title="Morpheus @ HTEC",
